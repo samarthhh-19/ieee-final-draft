e1d9341e6a3e29a4c434364f2ff04228cccd1f67
Samarth Dagar <dagarsamarth0808@gmail.com>
2025-07-13 11:52:04 +0530

    initial commit

---
+++ demo1/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Past4 from "./components/pastEvents"
+function App() {
+
+  return (
+    <>
+        <Past4/>
+    </>
+  )
+}
+
+export default App
---
+++ demo1/src/components/pastEvents.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import CardSwap, { Card } from "./CardSwap/CardSwap";
+import FadeContent from "./FadeContent/FadeContent";
+import Algoverse from "../assets/algoverse.png";
+import Pedal from "../assets/pedal.png";
+import Tensymp from "../assets/tensymp.png";
+import Dssywlc from "../assets/dssywlc.png";
+import useWindowWidth from "../hooks/useWindowWidth";
+
+const PastEvents = () => {
+  const width = useWindowWidth();
+  const cardSwapWidth = width < 768 ? 780 : 1000;
+
+  const boxes = [
+    {
+      img: Tensymp,
+      title: "Tensymp'24",
+      desc: "IEEE TENSYMP'24, is a premier symposium showcasing cutting-edge technological advancements, fostering global collaboration, and empowering researchers, professionals, and students through insightful sessions, workshops, and networking opportunities across diverse domains.",
+    },
+    {
+      img: Dssywlc,
+      title: "DSSYWLC'24",
+      desc: "IEEE DSSYWLC is a dynamic event focused on networking, knowledge-sharing, and community building through technical symposiums, discussions and cultural festivities.",
+    },
+    {
+      img: Algoverse,
+      title: "Algoverse 3.0",
+      desc: "IEEE NSUT AlgoVerse is a vibrant, community-driven initiative that provides an engaging and structured platform to thoroughly master DSA through consistent, daily problem-solving challenges like Problem of the Day (POTD), fostering both collaboration and growth.",
+    },
+    {
+      img: Pedal,
+      title: "Pedal Playground",
+      desc: "Pedal Playground, organized by IEEE NSUT in collaboration with Crescendo, is an interactive workshop exploring the art of sound design and audio synthesis through pedals. Dive into creative experimentation, music tech.",
+    },
+  ];
+
+  return (
+    <FadeContent blur={true} duration={900} easing="ease-out" initialOpacity={0}>
+      <section className="flex flex-col lg:flex-row w-full min-h-screen text-white bg-black overflow-hidden relative px-4 lg:px-0 ">
+        {/* Text Content */}
+        <div className="w-full lg:w-1/3 flex flex-col justify-center items-start px-2 py-6 lg:pl-12 lg:pr-8 z-10">
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-4 leading-tight">
+            Crafting Excellence: Pioneering Events by IEEE NSUT
+          </h1>
+          <p className="text-base sm:text-lg text-gray-300">
+            With a strong focus on excellence and innovation, IEEE NSUT's events provide truly unparalleled opportunities for meaningful networking, hands-on skill-building, immersive learning, and effective real-world problem-solving.
+          </p>
+        </div>
+
+        {/* Cards */}
+        <div className="w-full lg:w-2/3 flex items-center justify-center py-4 lg:py-0">
+          <div className="w-full h-full flex items-center justify-center">
+            <CardSwap
+              width={cardSwapWidth}
+              height={650}
+              cardDistance={90}
+              verticalDistance={70}
+              delay={4000}
+            >
+              {boxes.map((box, idx) => (
+                <Card key={idx}>
+                  <div className="flex flex-col h-full w-full overflow-hidden rounded-xl">
+                    <img
+                      src={box.img}
+                      alt={`box-${idx}`}
+                      className="h-3/5 w-full object-contain"
+                    />
+                    <div className="p-4 flex-1 flex flex-col">
+                      <h2 className="text-2xl font-semibold mb-2">{box.title}</h2>
+                      <p className="text-sm text-gray-300">{box.desc}</p>
+                    </div>
+                  </div>
+                </Card>
+              ))}
+            </CardSwap>
+          </div>
+        </div>
+      </section>
+    </FadeContent>
+  );
+};
+
+export default PastEvents;
